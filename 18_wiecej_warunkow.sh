11c0bd178666ce425d3f59923014621924be6658
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-07 15:46:43 +0100

    elif

--- 18_wiecej_warunkow.sh
+++ 18_wiecej_warunkow.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-if id basia1; then
+if id basia1
+then
 	echo UŻYTKOWNIK ISTNIEJE
 	echo HA HA HA
 	echo WSZYSTKO OK
+elif id basia2
+then
+	echo "Istnieje basia2"
+elif id basia3
+then
+	echo "Istnieje basia3"
 else
-	if id basia2; then
-		echo "Istnieje basia2"
-	else
-		echo "Uzytkownik nie istnieje"
-	fi
+	echo "Uzytkownik nie istnieje"
 fi
 
 echo KONIEC
