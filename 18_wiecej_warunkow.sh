dc574ec59683ffc5ee5235329f91da783f510dd5
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-07 16:08:45 +0100

    taby

--- 18_wiecej_warunkow.sh
+++ 18_wiecej_warunkow.sh
@@ -2,17 +2,17 @@
 
 if id basia1
 then
-	echo UŻYTKOWNIK ISTNIEJE
-	echo HA HA HA
-	echo WSZYSTKO OK
+    echo UŻYTKOWNIK ISTNIEJE
+    echo HA HA HA
+    echo WSZYSTKO OK
 elif id basia2
 then
-	echo "Istnieje basia2"
+    echo "Istnieje basia2"
 elif id basia3
 then
-	echo "Istnieje basia3"
+    echo "Istnieje basia3"
 else
-	echo "Uzytkownik nie istnieje"
+    echo "Uzytkownik nie istnieje"
 fi
 
 
