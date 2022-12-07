ca0ea3faac37414156d0a1d842df763cf4514bb7
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-07 14:49:26 +0100

    arytmetyczny

--- 14_arytmetyka.sh
+++ 14_arytmetyka.sh
@@ -3,5 +3,23 @@
 a=10
 b=20
 
-echo $(( a + b ))
+# zadziała, ale lepiej dać cudzysłów
+echo Suma: $(( a + b ))
+
+# OK
+echo "Suma: $(( a + b ))"
+
+# zadziała, ale może prowadzić do błędu
+echo "Suma: $(( $a + $b ))"
+
+# UWAGA, zmienna c nie jest zdefiniowana,
+# więc jako string wynosi "",
+# a jako liczba wynosi 0
+
+# OK - zadziała --> c wynosi 0
+echo "Suma: $(( a + b + c ))"
+
+# nie zadziała, c wynosi "", więc jest błąd składniowy
+echo "Suma: $(( $a + $b + $c ))"
+#    "Suma: $(( 10 + 20 + ))"
 
