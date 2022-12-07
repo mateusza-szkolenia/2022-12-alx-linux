23629bb60193c1ae4113565d83a3f97a91d5a0b2
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-07 12:46:02 +0100

    kropka

--- 12_skrypt.sh
+++ 12_skrypt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-source 99_konf
+# source 99_konf
+
+# pojedyncza kropka to polecnie source:
+
+. 99_konf
 
 echo "Nazywam się $imie, pracuję w $firma w mieście $miasto i prowadzę szkolenia o tematyce $szkolenie."
