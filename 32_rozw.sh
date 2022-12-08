09dab01ab01e0c038c44eef7a68c43634f25e207
Mateusz Adamowski <m.adamowski@alx.pl>
2022-12-08 15:28:28 +0100

    rozwiązanie

--- 32_rozw.sh
+++ 32_rozw.sh
@@ -4,8 +4,16 @@ for skrypt in *.sh
 do
     [ -x "$skrypt" ] && prawa=OK || prawa=brak
 
-    
+    # policzenie liczby linii
+    linie=$(wc -l < "$skrypt")
 
-    echo "Skrypt $skrypt: prawa $prawa "
+    # policzenie niepustych linii
+    niepuste=$(grep -c -v '^$' < "$skrypt")
+
+    grep -q TODO "$skrypt" && jest_TODO=tak || jest_TODO=nie
+
+    grep -q FIXME "$skrypt" && jest_FIXME=tak || jest_FIXME=nie
+
+    echo "Skrypt $skrypt: prawa $prawa, linii kodu: $linie (niepustych: $niepuste) (TODO: $jest_TODO, FIXME: $jest_FIXME)"
 done
 
